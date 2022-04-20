738524e2a3c7df820acef008c75fdc2d9fb54ce8
Christoffer Abildskov Thomsen <60384328+Cathomsen@users.noreply.github.com>
2022-04-20 09:18:50 +0200

    Push notification - doesnt work yet

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -2,3 +2,8 @@ import { hydrate } from "react-dom";
 import { RemixBrowser } from "remix";
 
 hydrate(<RemixBrowser />, document);
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(function () {
+    console.log("Service Worker Registered");
+  });
+}
